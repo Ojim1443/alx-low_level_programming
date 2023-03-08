9b3352a70534f524540601ecd992f29ea8a70ce2
Ojim1443 <osinachiojim21@gmail.com>
2023-03-08 05:41:46 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,19 +3,19 @@
  * _strstr - Entry point
  * @haystack: input
  * @needle: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle);
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *I = haystack;
+		char *l = haystack;
 		char *p = needle;
 
-		while (*I == *p && *p != '\0')
+		while (*l == *p && *p != '\0')
 		{
-			I++
-			p++
+			l++;
+			p++;
 		}
 		if (*p == '\0')
 			return (haystack);
