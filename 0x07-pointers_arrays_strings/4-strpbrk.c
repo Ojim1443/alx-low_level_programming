26080cc7f1f46e357c2732aa957d8b81b0f46018
Ojim1443 <osinachiojim21@gmail.com>
2023-03-08 05:08:50 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,12 +9,12 @@ char *_strpbrk(char *s, char *accept)
 {
 	int k;
 
-	while (*S)
+	while (*s)
 	{
 		for (k = 0; accept[k]; k++)
 		{
-		if s[i]  == accept[c];
-		return (s + i);
+		if (*s == accept[k])
+		return (s);
 		}
 	s++;
 	}
