398f6da483b192f16c7c7a42d24a01d386653cb4
Ojim1443 <osinachiojim21@gmail.com>
2023-03-09 04:00:51 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,15 +4,13 @@
  * @s: Pointer block of memory to fill.
  * Return: strlen_recursion
  */
+
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	/*Base condition*/
 
-	if (*S == '\0')
-	{
-		return (0);
-		
-		else
-		return (1 + _strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+	return (0);
+	else
+	return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
 }
