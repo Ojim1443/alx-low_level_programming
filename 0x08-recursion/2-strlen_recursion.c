ac4e968ef35d6167d6c840a9683c1c164561fe3f
Ojim1443 <osinachiojim21@gmail.com>
2023-03-09 02:20:42 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _strlen_recursion - The length of a string.
+ * @s: Pointer block of memory to fill.
+ * Return: strlen_recursion
  */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*S)
+	if (*S == '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (0);
+		
+		else
+		return (1 + _strlen_recursion(s + 1);
 	}
 }
